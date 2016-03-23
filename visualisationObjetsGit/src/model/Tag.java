e656b62390a86212296767264a6bde8a3ab9ec73
COT Bastien <COT Bastien>
2016-03-23 21:09:14 +0100

    Modif Tag BufferReader

--- visualisationObjetsGit/src/model/Tag.java
+++ visualisationObjetsGit/src/model/Tag.java
@@ -1,7 +1,10 @@
 package model;
 
 import java.io.File;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Tag extends GitObject{
     
@@ -9,6 +12,26 @@ public class Tag extends GitObject{
         
         super(_file);
         
+        //lire le tag avec filereading
+
     }
+       public static void main(String[] argv) throws IOException
+  {
+      BufferedReader lecteurAvecBuffer=null;
+    String ligne;
     
-}
+    try
+      {
+	lecteurAvecBuffer = new BufferedReader
+	  (new FileReader(argv[0]));
+      }
+    catch(FileNotFoundException exc)
+      {
+	System.out.println("Erreur d'ouverture");
+      }
+    
+    while ((ligne = lecteurAvecBuffer.readLine())!=null)
+      System.out.println(ligne);
+    lecteurAvecBuffer.close();
+  }
+} 
